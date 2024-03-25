54db5e3893d0159cb1696f01f5afb0bceee6ff50
Humberto Mejia <hmejia03@email.cpcc.edu>
2024-03-25 00:24:03 -0400

    Revert "Update HelloWorld.java"
    This reverts commit cdc8ff7720cc4b801eaa496820b69bcee6750010.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,9 @@
 
 public class HelloWorld
 {
-   public static void main(String[] args)
+      public static void main(String[] args)
       {
-         System.out.println("Hello World!");
-         System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
+            System.out.println("Hello World!");
+            }
       }
-}
       
